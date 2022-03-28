9ec80f00da268dcb2b9e4d92c66003d414a8eb62
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2022-03-27 18:19:02 -0600

    tests: lib/bin: create lib/bin tests dir
    Create lib/bin test dirs to separate tests for library and binary
    
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -4,12 +4,12 @@ find_program(genhtml genhtml REQUIRED)
 
 SET(CMAKE_C_FLAGS "--coverage")
 
-function(coverage_target build_dir extract_pattern target_dependency)
+function(coverage_target build_dir exclude_pattern target_dependency)
 	add_custom_target(coverage
 		COMMAND ${CMAKE_COMMAND} -E remove_directory ${build_dir}/coverage
 		COMMAND ${CMAKE_COMMAND} -E make_directory ${build_dir}/coverage
 		COMMAND ${lcov} --directory ${build_dir} --capture --output-file coverage/coverage.info
-		COMMAND ${lcov} --directory ${build_dir} --extract coverage/coverage.info ${extract_pattern} --output-file coverage/coverage.info
+		COMMAND ${lcov} --directory ${build_dir} --remove coverage/coverage.info ${exclude_pattern} --output-file coverage/coverage.info
 		COMMAND ${genhtml} -o ${build_dir}/coverage ${build_dir}/coverage/coverage.info
 		DEPENDS ${target_dependency}
 		)
